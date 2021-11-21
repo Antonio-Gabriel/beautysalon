8173ea36256589d70a30bad115a040e101be778d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-21 18:18:24 +0100

    scripts: remove menu after click on link function

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
+const links = document.querySelectorAll("nav ul li a");
+
 for (const element of toggle) {
   element.addEventListener("click", function () {    
     nav.classList.toggle("isOpen")
+  });
+}
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("isOpen")
   });
 }
