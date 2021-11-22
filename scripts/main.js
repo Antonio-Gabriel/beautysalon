b1dc4b1d97b040970ec3cfb27e76fa9fa58fa5ed
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-22 15:50:02 +0100

    add scrollreavel script

--- scripts/main.js
+++ scripts/main.js
@@ -9,34 +9,52 @@ const navHeight = header.offsetHeight;
 const links = document.querySelectorAll("nav ul li a");
 
 for (const element of toggle) {
-  element.addEventListener("click", function () {    
-    nav.classList.toggle("isOpen")
+  element.addEventListener("click", function () {
+    nav.classList.toggle("isOpen");
   });
 }
 
 // Close menu after click on menu
 for (const link of links) {
   link.addEventListener("click", function () {
-    nav.classList.remove("isOpen")
+    nav.classList.remove("isOpen");
   });
 }
 
 // Srolling effect
 window.addEventListener("scroll", function () {
-  if(window.scrollY >= navHeight) {
-    header.classList.add("has-scroll")
+  if (window.scrollY >= navHeight) {
+    header.classList.add("has-scroll");
   } else {
-    header.classList.remove("has-scroll")
+    header.classList.remove("has-scroll");
   }
 });
 
-
 // Teslemonial Slider Swiper
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
 });
+
+/* Scroll Reveal */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+  #home .text, #home .image
+  #about .text, #about .image
+  #services header, #services .card
+  #testemonials header, #testemonials .testemonial
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+);
